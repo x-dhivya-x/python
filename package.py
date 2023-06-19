3c9b6c513e4de8ea733936de4b0250a5868a77d3
Dhivya <dhivyavpy@gmail.com>
2023-06-19 12:52:11 +0530

    Success

--- package.py
+++ package.py
@@ -1,2 +1,2 @@
-from college.department import dept_func 
+from college.department import dept_func 	#here college is a package and it contains variety of modules.here we access department module
 dept_func()
