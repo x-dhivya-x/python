9eecd9f0457a540dcf1b14fab2a1937f6bd74629
Dhivya <dhivyavpy@gmail.com>
2023-06-22 19:51:52 +0530

    Success

--- 2D_list.py
+++ 2D_list.py
@@ -1,8 +1,10 @@
-matrix=[   
- 	[1,9,6],
- 	[4,8,6],
- 	[3,5,2]
-
-]
-print(matrix)
-print(matrix[0][2])
+l1=[1,2,3]
+l2=[4,5,6]
+l3=[7,8,9]
+matrix=[l1,l2,l3]
+print(f"{l1}\n{l2}\n{l3}")
+position=input("Enter the position to hide your things: ")
+row=int(position[0])
+column=int(position[1])
+result=matrix[row-1][column-1]='X'
+print(f"{l1}\n{l2}\n{l3}")
